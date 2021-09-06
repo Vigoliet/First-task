4ee8f937eeeaac24ba7da699b5fe39d85cc5ddd0
vigor-lundkvist <vigor.lundkvist@live.se>
2021-09-06 12:01:18 +0200

    Skickat

---
+++ Uppgift1/Uppgift1/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Vigor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Mina uppgifter:");
+
+            //Uppgift A - Happy path
+            //Lagar data, deklarera variabler
+            int amount_of_questions = 3;
+            string[] food = new string[amount_of_questions];
+
+            //en array är positionsbaserad
+            //dvs man räknar med start från (0) [0, 1, 2]
+
+            //ställ frågor ett antal gånger
+
+            for (int i = 0; i < food.Length; i++)
+            {
+                Console.WriteLine("Ange en maträtt:");
+                food[i] = Console.ReadLine();
+            }
+
+            //skriv ut alla maträtterna
+            for (int i = 0; i < food.Length; i++)
+            {
+                Console.Write(food[i] + " ");
+            }
+
+            //Skriv ut alla maträtterna med antal tecken, en per rad
+            for (int i = 0; i < food.Length; i++)
+            {
+                Console.WriteLine(food[i] + " (" + food[i].Length + ")");
+            }
+
+
+            //Hur hittar jag den längsta maträtten?
+
+            string longest_food = [0];
+            if (food[0].Length < food[1].Length)
+            {
+                longest_food = food[2];
+            }
+            if (food[1].Length < food[2].Length)
+            {
+                longest_food = food[2];
+            }
+
+            Console.WriteLine("Longest food: " + longest_food);
+            Console.ReadLine();
+
+            for (int i = 0; i < food.Length - 1; i++)
+            {
+                if (food[1].Length < food[i + 1].Length)
+                {
+                    longest_food = food[i + 1];
+                }
+            }
+
+        }
+    }
+}
